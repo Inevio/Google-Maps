2b644d93e6a51b89d631b3afbe6d8a7906a1e357
Jesús Carpintero Rodríguez <j.carpintero@alumnos.upm.es>
2016-06-07 10:33:20 +0200

    Window Size

--- static/script.js
+++ static/script.js
@@ -1,9 +1,23 @@
 
-    var win    = $( this );
-    var width  = api.tool.desktopWidth();
-    var height = api.tool.desktopHeight() - 70;
-    var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
-    var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
+  var win    = $( this );
+  var width;
+  var height;
+  var minWidth  = 629;
+  var minHeight = 600;
+  var maxWidth = api.tool.desktopWidth();
+  var maxHeigth = api.tool.desktopHeight() - 70;
+
+
+  if (minHeight > maxHeigth || minWidth > maxWidth) {
+    width = maxWidth;
+    height = maxHeigth;
+
+  }else {
+    width = minWidth;
+    height = minHeight;
+  }
+  var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
+  var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
     var windowObject = api.popup( 'https://maps.google.com', width, height).render();
 
     var timer = setInterval( function(){
